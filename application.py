598f8d4d6e286bd6c8e2fdc9a06397fa8e23c3f0
Somit Sinha <somitsinha108@gmail.com>
2021-04-06 18:04:21 +0530

    Updated the JSON output to include page pointers to previous and next page as well

--- application.py
+++ application.py
@@ -1,46 +1,69 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from replacements import current_id_replacements, verse_replacements, synonyms_replacements, translation_replacements, purport_replacements
 from pathlib import Path
 
 
-_index = 1
+def parser():
+    url = f'https://vanisource.org/wiki/SB_1.1.{_index}'
 
-url = f'https://vanisource.org/wiki/SB_1.1.{_index}'
+    _source = requests.get(url)
 
-_source = requests.get(url)
+    soup = BeautifulSoup(_source.content, 'html.parser')
 
-soup = BeautifulSoup(_source.content, 'html.parser')
+    b_tags = soup.find_all("b")
 
-current_id = str(soup.find("h1", {"id": "firstHeading"}))
-current_id = current_id_replacements(current_id)
+    headers = str(b_tags[len(b_tags)-1])
+    headers = headers.replace('<b>', '')
+    headers = headers.replace('</b>', '')
+    headers = list(headers.split('-'))
+    
+    pointers = []
+    for header in headers:
+        head = list(list(header.split('>'))[1].split('<'))[0]
+        pointers.append(head)
+    
+    # print(headers)
+    # print(pointers)
 
-verse = str(soup.find("div", {"class":"verse"}))
-verse = verse_replacements(verse)
-verse_entry = [{"roman": verse, "isProse": False}]
+    current_id = str(soup.find("h1", {"id": "firstHeading"}))
+    current_id = current_id_replacements(current_id)
 
-synonyms = str(soup.find("div", {"class": "synonyms"}))
-synonyms = synonyms_replacements(synonyms)
+    if _index==1:
+        navigation = {"current_id": current_id, "previous_id": None, "next_id": pointers[0]}
+    else:
+        navigation = {"current_id": current_id, "previous_id": pointers[0], "next_id": pointers[1]}
 
-translation = str(soup.find("div", {"class":"translation"}))
-translation = translation_replacements(translation)
+    verse = str(soup.find("div", {"class":"verse"}))
+    verse = verse_replacements(verse)
+    verse_entry = [{"roman": verse, "isProse": False}]
 
-purport = str(soup.find("div", {"class": "purport"}))
-purport = purport_replacements(purport)
-purport_paras = list(filter(None, list(purport.split('\n'))))
-purport_entry = [{"type": "regular", "text": para} for para in purport_paras]
+    synonyms = str(soup.find("div", {"class": "synonyms"}))
+    synonyms = synonyms_replacements(synonyms)
+
+    translation = str(soup.find("div", {"class":"translation"}))
+    translation = translation_replacements(translation)
+
+    purport = str(soup.find("div", {"class": "purport"}))
+    purport = purport_replacements(purport)
+    purport_paras = list(filter(None, list(purport.split('\n'))))
+    purport_entry = [{"type": "regular", "text": para} for para in purport_paras]
 
 
 
-knowledge = {"page_id": current_id, "verse": verse_entry, "synonyms": synonyms, "translation": translation, "purport": purport_entry}
+    knowledge = {"page_info": navigation, "verse": verse_entry, "synonyms": synonyms, "translation": translation, "purport": purport_entry}
 
 
-print(knowledge)
-print(type(knowledge))
+    print(knowledge)
+    print(type(knowledge))
 
-if Path(f'/home/somit/Projects/web-scraping/SB/1/1/{_index}.json').is_file():
-    with open(f'/home/somit/Projects/web-scraping/SB/1/1/{_index}.json', 'w') as json_file:
-        print(knowledge, file=json_file)
-else:
-    with open(f'/home/somit/Projects/web-scraping/SB/1/1/{_index}.json', 'x') as json_file:
-        print(knowledge, file=json_file)
+    if Path(f'/home/somit/Projects/web-scraping/SB/1/1/{_index}.json').is_file():
+        with open(f'/home/somit/Projects/web-scraping/SB/1/1/{_index}.json', 'w') as json_file:
+            print(knowledge, file=json_file)
+    else:
+        with open(f'/home/somit/Projects/web-scraping/SB/1/1/{_index}.json', 'x') as json_file:
+            print(knowledge, file=json_file)
+
+for _index in range(1,24):
+    parser()
